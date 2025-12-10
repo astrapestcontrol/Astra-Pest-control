5481f7ae9966bcb0e89eec0186a4b00ee172f0d8
Astra Pest Control <astrapestcontrol@gmail.com>
2025-12-10 18:39:30 +0000

    Critical performance optimizations: lazy loading, caching, preconnect

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
@@ -6,35 +6,37 @@ import StickyCTA from './components/StickyCTA';
 import ScrollToTopButton from './components/ScrollToTopButton';
 import Footer from './components/Footer';
 import SmoothScroll from './components/SmoothScroll';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Cleaning from './pages/Cleaning';
-import PestControl from './pages/PestControl';
-import About from './pages/About';
-import FAQ from './pages/FAQ';
-import HintsTips from './pages/HintsTips';
-import Contact from './pages/Contact';
-import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
-import Brisbane from './pages/Brisbane';
-import GoldCoast from './pages/GoldCoast';
 
-// Cleaning Service Pages
-import CarpetCleaning from './pages/cleaning/CarpetCleaning';
-import TileGroutCleaning from './pages/cleaning/TileGroutCleaning';
-import UpholsteryCleaning from './pages/cleaning/UpholsteryCleaning';
-import LeatherCleaning from './pages/cleaning/LeatherCleaning';
-import MouldRemoval from './pages/cleaning/MouldRemoval';
-import RugCleaning from './pages/cleaning/RugCleaning';
-import VehicleCleaning from './pages/cleaning/VehicleCleaning';
-import FloodExtraction from './pages/cleaning/FloodExtraction';
-import CommercialCleaning from './pages/cleaning/CommercialCleaning';
-import BondCleaning from './pages/cleaning/BondCleaning';
-import MattressCleaning from './pages/cleaning/MattressCleaning';
-import CarpetProtection from './pages/cleaning/CarpetProtection';
-import EncapsulationCleaning from './pages/cleaning/EncapsulationCleaning';
-import CarpetRepairs from './pages/cleaning/CarpetRepairs';
-import DuoCleaning from './pages/cleaning/DuoCleaning';
+// Lazy load all pages
+const Home = lazy(() => import('./pages/Home'));
+const Services = lazy(() => import('./pages/Services'));
+const Cleaning = lazy(() => import('./pages/Cleaning'));
+const PestControl = lazy(() => import('./pages/PestControl'));
+const About = lazy(() => import('./pages/About'));
+const FAQ = lazy(() => import('./pages/FAQ'));
+const HintsTips = lazy(() => import('./pages/HintsTips'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Blog = lazy(() => import('./pages/Blog'));
+const BlogPost = lazy(() => import('./pages/BlogPost'));
+const Brisbane = lazy(() => import('./pages/Brisbane'));
+const GoldCoast = lazy(() => import('./pages/GoldCoast'));
+
+// Lazy load cleaning service pages
+const CarpetCleaning = lazy(() => import('./pages/cleaning/CarpetCleaning'));
+const TileGroutCleaning = lazy(() => import('./pages/cleaning/TileGroutCleaning'));
+const UpholsteryCleaning = lazy(() => import('./pages/cleaning/UpholsteryCleaning'));
+const LeatherCleaning = lazy(() => import('./pages/cleaning/LeatherCleaning'));
+const MouldRemoval = lazy(() => import('./pages/cleaning/MouldRemoval'));
+const RugCleaning = lazy(() => import('./pages/cleaning/RugCleaning'));
+const VehicleCleaning = lazy(() => import('./pages/cleaning/VehicleCleaning'));
+const FloodExtraction = lazy(() => import('./pages/cleaning/FloodExtraction'));
+const CommercialCleaning = lazy(() => import('./pages/cleaning/CommercialCleaning'));
+const BondCleaning = lazy(() => import('./pages/cleaning/BondCleaning'));
+const MattressCleaning = lazy(() => import('./pages/cleaning/MattressCleaning'));
+const CarpetProtection = lazy(() => import('./pages/cleaning/CarpetProtection'));
+const EncapsulationCleaning = lazy(() => import('./pages/cleaning/EncapsulationCleaning'));
+const CarpetRepairs = lazy(() => import('./pages/cleaning/CarpetRepairs'));
+const DuoCleaning = lazy(() => import('./pages/cleaning/DuoCleaning'));
 
 import './App.css';
 
@@ -68,36 +70,38 @@ function App() {
           <StickyCTA />
           <ScrollToTopButton />
           <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/cleaning" element={<Cleaning />} />
-              <Route path="/cleaning/carpet-cleaning" element={<CarpetCleaning />} />
-              <Route path="/cleaning/tile-grout" element={<TileGroutCleaning />} />
-              <Route path="/cleaning/upholstery" element={<UpholsteryCleaning />} />
-              <Route path="/cleaning/leather" element={<LeatherCleaning />} />
-              <Route path="/cleaning/mould" element={<MouldRemoval />} />
-              <Route path="/cleaning/rug" element={<RugCleaning />} />
-              <Route path="/cleaning/vehicles" element={<VehicleCleaning />} />
-              <Route path="/cleaning/flood" element={<FloodExtraction />} />
-              <Route path="/cleaning/commercial" element={<CommercialCleaning />} />
-              <Route path="/cleaning/bond" element={<BondCleaning />} />
-              <Route path="/cleaning/mattress" element={<MattressCleaning />} />
-              <Route path="/cleaning/protection" element={<CarpetProtection />} />
-              <Route path="/cleaning/encapsulation" element={<EncapsulationCleaning />} />
-              <Route path="/cleaning/repairs" element={<CarpetRepairs />} />
-              <Route path="/cleaning/duo-system" element={<DuoCleaning />} />
-              <Route path="/cleaning/:service" element={<Services />} />
-              <Route path="/pest-control" element={<PestControl />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/hints-tips" element={<HintsTips />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:id" element={<BlogPost />} />
-              <Route path="/brisbane" element={<Brisbane />} />
-              <Route path="/gold-coast" element={<GoldCoast />} />
-            </Routes>
+            <Suspense fallback={<div style={{height:'50vh',display:'flex',alignItems:'center',justifyContent:'center'}}>Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/cleaning" element={<Cleaning />} />
+                <Route path="/cleaning/carpet-cleaning" element={<CarpetCleaning />} />
+                <Route path="/cleaning/tile-grout" element={<TileGroutCleaning />} />
+                <Route path="/cleaning/upholstery" element={<UpholsteryCleaning />} />
+                <Route path="/cleaning/leather" element={<LeatherCleaning />} />
+                <Route path="/cleaning/mould" element={<MouldRemoval />} />
+                <Route path="/cleaning/rug" element={<RugCleaning />} />
+                <Route path="/cleaning/vehicles" element={<VehicleCleaning />} />
+                <Route path="/cleaning/flood" element={<FloodExtraction />} />
+                <Route path="/cleaning/commercial" element={<CommercialCleaning />} />
+                <Route path="/cleaning/bond" element={<BondCleaning />} />
+                <Route path="/cleaning/mattress" element={<MattressCleaning />} />
+                <Route path="/cleaning/protection" element={<CarpetProtection />} />
+                <Route path="/cleaning/encapsulation" element={<EncapsulationCleaning />} />
+                <Route path="/cleaning/repairs" element={<CarpetRepairs />} />
+                <Route path="/cleaning/duo-system" element={<DuoCleaning />} />
+                <Route path="/cleaning/:service" element={<Services />} />
+                <Route path="/pest-control" element={<PestControl />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/hints-tips" element={<HintsTips />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="/brisbane" element={<Brisbane />} />
+                <Route path="/gold-coast" element={<GoldCoast />} />
+              </Routes>
+            </Suspense>
           </main>
           <Footer />
         </div>
